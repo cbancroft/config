eadf841e15febc4d38add7e7218e7748ea53aa21
Charles Bancroft <bancroftc@ainfosec.com>
2024-04-17 13:41:52 -0400

    Delete some unnecessary files

--- laptopscreen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#This script disables my 2 external screens and enables my laptop display
-xrandr --output DP-1 --off --output DP-2 --off --output VGA-1 --off
-xrandr --output LVDS-1 --auto --primary --pos 0x0
--- vgascreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#This script enables my 2 external screens and disables my laptop display
-xrandr --output VGA-0 --auto --right-of LVDS-0
